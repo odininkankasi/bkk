bcf097c57889786ce0ff26b89857d407f7a2f797
Auto Update <auto@update.com>
2026-07-21 00:20:54 +0300

    Feat: Uygulama PWA (Progressive Web App) formatina cevirildi (Manifest, ikon ve Service Worker eklendi)

---
+++ sw.js
@@ -0,0 +1,53 @@
+const CACHE_NAME = 'bkk-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './icon.png',
+  './manifest.json',
+  'https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,400;0,600;0,700;1,400&family=Outfit:wght@300;400;500;600&display=swap'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request).then(
+          function(response) {
+            // Check if we received a valid response
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            // IMPORTANT: Clone the response. A response is a stream
+            // and because we want the browser to consume the response
+            // as well as the cache consuming the response, we need
+            // to clone it so we have two streams.
+            var responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(function(cache) {
+                // Don't cache the google sheets API calls because they are dynamic
+                if (!event.request.url.includes('docs.google.com')) {
+                   cache.put(event.request, responseToCache);
+                }
+              });
+
+            return response;
+          }
+        );
+      })
+  );
+});
